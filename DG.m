fd8f569265e887f1a99b8f8386ccddf422feff3a
ITHENOA <79358150+ITHENOA@users.noreply.github.com>
2024-01-01 16:27:50 +0330

    1

---
+++ DG.m
@@ -0,0 +1,7 @@
+% Gradian Desent Training
+% fuzzy system with :
+%       "product" inference engine,
+%       "singleton" fuzzifier, 
+%       "centeroid" defuzzifier,
+%       "Gaussian" membership function.
+gradient()
